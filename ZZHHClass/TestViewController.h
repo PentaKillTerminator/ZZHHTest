66cca6991d277b06c2df8d97fa273862e85c561d
Mr.Z <1365986695@qq.com>
2019-01-08 14:44:41 +0800

    添加class文件

---
+++ ZZHHClass/TestViewController.h
@@ -0,0 +1,14 @@
+//
+//  TestViewController.h
+//  vccvc
+//
+//  Created by Mr.Z on 2019/1/8.
+//  Copyright © 2019 lonch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestViewController : UIViewController
+- (void)test;
+@end
+
---
+++ ZZHHClass/TestViewController.m
@@ -0,0 +1,36 @@
+//
+//  TestViewController.m
+//  vccvc
+//
+//  Created by Mr.Z on 2019/1/8.
+//  Copyright © 2019 lonch. All rights reserved.
+//
+
+#import "TestViewController.h"
+
+@interface TestViewController ()
+
+@end
+
+@implementation TestViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)test{
+    NSLog(@"这是一个pod测试工程");
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
